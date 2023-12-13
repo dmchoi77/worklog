3592bb9a90b0b21706cd3a8cbe91e8de36565a06
dm.choi <dm.choi@logiall.com>
2023-12-13 21:11:08 +0900

    update: useInput 작성

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+const useInput = () => {
+  const [input, setInput] = useState<string>('');
+
+  const handleInput = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => setInput(value);
+
+  const reset = () => setInput('');
+
+  return { input, handleInput, reset };
+};
+
+export default useInput;
