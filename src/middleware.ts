a92c6992f498667ee0e2c31b565e2582e56eef23
dm.choi <dm.choi@logiall.com>
2023-12-13 20:13:52 +0900

    fix: middleware 라우팅 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,10 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from './constants/cookie';
-import { removeCookie } from './utils/cookie';
 
 export async function middleware(request: NextRequest) {
   const accessToken = request.cookies.get(ACCESS_TOKEN)?.value;
   const refreshToken = request.cookies.get(REFRESH_TOKEN)?.value;
-
-  const nextResponse = NextResponse.next();
 
   if (request.nextUrl.pathname === '/login' && refreshToken) {
     return NextResponse.redirect(new URL('/', request.url));
@@ -17,26 +14,11 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  // if (refreshToken && !accessToken) {
-  //   try {
-  //     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/reissue`, {
-  //       method: 'POST',
-  //       headers: {
-  //         Authorization: `Bearer ${refreshToken}`,
-  //       },
-  //     });
-  //     const data = await response.json();
-  //     if (data.status === 200) {
-  //       nextResponse.cookies.set(REFRESH_TOKEN, data.data?.refreshToken);
-  //       nextResponse.cookies.set(ACCESS_TOKEN, data.data?.accessToken);
-  //       return nextResponse;
-  //     } else throw data;
-  //   } catch (e) {
-  //     console.log('🚀 ~ file: middleware.ts:17 ~ middleware ~ e:', e);
-  //   }
-  // }
+  if (request.nextUrl.pathname !== '/login' && !accessToken) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 }
 
 export const config = {
-  matcher: ['/', '/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/', '/((?!api|_next/static|_next/image|favicon.ico|signin).*)'],
 };
