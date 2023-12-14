a6575e592e2cfcf80ead0783fee8b4c31003b8b4
dm.choi <dm.choi@logiall.com>
2023-12-14 16:37:07 +0900

    fix: 미들웨어 쿠키 에러  수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,22 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-import { REFRESH_TOKEN } from '~/constants/cookie';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '~/constants/cookie';
 
 export async function middleware(request: NextRequest) {
-  // const accessToken = request.cookies.get(ACCESS_TOKEN)?.value;
+  const accessToken = request.cookies.get(ACCESS_TOKEN)?.value;
   const refreshToken = request.cookies.get(REFRESH_TOKEN)?.value;
+  const isAuth = accessToken && refreshToken
 
-  if (request.nextUrl.pathname === '/login' && refreshToken) {
+  if (request.nextUrl.pathname === '/login' && isAuth) {
     return NextResponse.redirect(new URL('/today', request.url));
   }
 
-  if (request.nextUrl.pathname === '/' && !refreshToken) {
+  if (request.nextUrl.pathname === '/' && !isAuth) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  if (request.nextUrl.pathname !== '/login' && !refreshToken) {
+  if (request.nextUrl.pathname !== '/login' && !isAuth) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 }
