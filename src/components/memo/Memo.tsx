f95dcc2ca064b39fa54d3bd33c0ce570e3b9b94e
dm.choi <dm.choi@logiall.com>
2023-12-15 01:10:04 +0900

    feat: 메모 수정/삭제 추가

--- src/components/memo/Memo.tsx
+++ src/components/memo/Memo.tsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
+
+import { Box, Divider } from '@mui/material';
+
 import { Draggable } from 'react-beautiful-dnd';
 
 import styled from '@emotion/styled';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+
+import useDebounce from '~/hooks/useDebounce';
+import { useDeleteMemo, useUpdateMemo } from '~/queries/memo';
 
 interface IContainer {
   isDragging: boolean;
 }
 
 const Container = styled.div<IContainer>`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
@@ -14,7 +24,7 @@ const Container = styled.div<IContainer>`
   gap: 8px;
   border: 1px solid lightgrey;
   border-radius: 8px;
-  padding: 8px;
+  padding: 12px;
   margin-bottom: 8px;
   height: auto;
   background-color: ${(props) => (props.isDragging ? 'lightgreen' : '#ffffff')};
@@ -33,17 +43,57 @@ interface IMemoProps {
 }
 
 const Memo = ({ index, task }: IMemoProps) => {
+  const [visibleBtn, setVisibleBtn] = useState(false);
+  const [editable, setEditable] = useState(false);
+  const { mutate: updateMemo } = useUpdateMemo();
+  const { mutate: deleteMemo } = useDeleteMemo();
+
+  const debounceUpdateMemo = useDebounce((e) => updateMemo({ content: e.target.innerHTML, id: Number(task.id) }), 300);
+
   return (
     <Draggable draggableId={task.id} index={index}>
       {(provided, snapshot) => (
         <Container
+          key={task.id}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           isDragging={snapshot.isDragging}
+          onMouseOver={() => setVisibleBtn(true)}
+          onMouseOut={() => setVisibleBtn(false)}
         >
-          <span css={{ fontSize: 15, fontWeight: 700 }}>{task.id}</span>
-          {task.content}
+          {visibleBtn && (
+            <Box
+              css={{
+                display: 'flex',
+                position: 'absolute',
+                marginRight: 8,
+                right: 0,
+                borderRadius: 4,
+                boxShadow: 'rgba(15, 15, 15, 0.1) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 2px 4px',
+              }}
+            >
+              {/* <EditNoteIcon
+                css={{ borderRadius: 6, background: '#ffffff' }}
+                onClick={() => setEditable(true)}
+                onBlur={() => setEditable(false)}
+              /> */}
+              <Divider css={{ width: 1, background: 'rgba(15, 15, 15, 0.1)' }} />
+              <DeleteIcon
+                css={{ borderRadius: 6, background: '#ffffff' }}
+                onClick={() => deleteMemo({ id: Number(task.id) })}
+              />
+            </Box>
+          )}
+          <Box
+            contentEditable={editable}
+            suppressContentEditableWarning
+            onClick={() => setEditable(true)}
+            onBlur={() => setEditable(false)}
+            onInput={debounceUpdateMemo}
+          >
+            {task.content}
+          </Box>
         </Container>
       )}
     </Draggable>
