c99ad7b9ca32ca40b8be85e31d99cf8ec22f7c1d
dm.choi <dm.choi@logiall.com>
2023-12-25 00:25:37 +0900

    chore: 홈 제거, 404 페이지 추가

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+export default function Custom404() {
+  return (
+    <div
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <span>잘못된 경로입니다.</span>
+    </div>
+  );
+}
