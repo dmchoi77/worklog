0b5815571267ce8d60285ee8606a67a300dd1756
dm.choi <dm.choi@logiall.com>
2023-12-24 21:58:38 +0900

    chore: 운영환경에서 console 제거

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  compiler: {
+    removeConsole: isProd ? true : false,
+  },
 };
 
 module.exports = nextConfig;
