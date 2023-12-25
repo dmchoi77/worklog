31279a21f27a1abf67217d1953ee09a720bb6cb7
dm.choi <dm.choi@logiall.com>
2023-12-26 00:16:56 +0900

    fix(http): 리프레시 토큰 갱신 실패 처리

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,13 +1,13 @@
 import * as https from 'https';
 
-import axios, { InternalAxiosRequestConfig } from 'axios';
+import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
 import { getRemainExp } from './decodeJWT';
 
 import { ACCESS_TOKEN, REFRESH_TOKEN, TEN_HOURS } from '~/constants/cookie';
 import { ICommonResponse } from '~/types/apis/common.types';
 import { ILoginResponse } from '~/types/apis/user.types';
-import { getCookie, setCookie } from '~/utils/cookie';
+import { getCookie, removeCookie, setCookie } from '~/utils/cookie';
 
 const headers: Readonly<Record<string, string | boolean>> = {
   Accept: 'application/json',
@@ -51,37 +51,45 @@ http.interceptors.response.use(
       const refreshToken = getCookie(REFRESH_TOKEN);
       const accessToken = getCookie(ACCESS_TOKEN);
       if (accessToken && !refreshToken) return (location.href = 'login');
-      const response = await axios.post<ICommonResponse<ILoginResponse>>(
-        '/users/reissue',
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${refreshToken}`,
+      try {
+        const response = await axios.post<ICommonResponse<ILoginResponse>>(
+          '/users/reissue',
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${refreshToken}`,
+            },
+            baseURL: process.env.NEXT_PUBLIC_API_URL,
           },
-          baseURL: process.env.NEXT_PUBLIC_API_URL,
-        },
-      );
-      if (response.status === 403) return (location.href = 'login');
-      const { accessToken: newAccessToken, refreshToken: newRefreshToken } = response.data.data as ILoginResponse;
+        );
+        if (response.status === 403) return (location.href = 'login');
+        const { accessToken: newAccessToken, refreshToken: newRefreshToken } = response.data.data as ILoginResponse;
 
-      if (response.status === 200) {
-        setCookie(ACCESS_TOKEN, newAccessToken, {
-          secure: true,
-          path: '/',
-          // maxAge: getRemainExp(newAccessToken),
-        });
-        setCookie(REFRESH_TOKEN, newRefreshToken, {
-          secure: true,
-          path: '/',
-          maxAge: getRemainExp(newRefreshToken),
-        });
-        originalRequest.headers = {
-          Authorization: `Bearer ${newAccessToken}`,
-        };
-        http.defaults.headers.common = {
-          Authorization: `Bearer ${newAccessToken}`,
-        };
-        return http(originalRequest);
+        if (response.status === 200) {
+          setCookie(ACCESS_TOKEN, newAccessToken, {
+            secure: true,
+            path: '/',
+            // maxAge: getRemainExp(newAccessToken),
+          });
+          setCookie(REFRESH_TOKEN, newRefreshToken, {
+            secure: true,
+            path: '/',
+            maxAge: getRemainExp(newRefreshToken),
+          });
+          originalRequest.headers = {
+            Authorization: `Bearer ${newAccessToken}`,
+          };
+          http.defaults.headers.common = {
+            Authorization: `Bearer ${newAccessToken}`,
+          };
+          return http(originalRequest);
+        }
+      } catch (error: any) {
+        if ([401, 403].includes(error.response.status)) {
+          removeCookie(ACCESS_TOKEN);
+          removeCookie(REFRESH_TOKEN);
+          return (location.href = '/login');
+        }
       }
     }
     throw error.response.data;
