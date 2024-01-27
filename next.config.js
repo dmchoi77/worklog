012715afed7a29468a1b1136634373c650c91bfb
최동민 <dm.choi@logiall.com>
2024-01-27 19:20:01 +0900

    chore: next.config poweredByHeader 추가

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  poweredByHeader: false,
   compiler: {
     removeConsole: isProd ? true : false,
   },
