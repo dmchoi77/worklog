210b6e7bd6c33494d4b2aabbb24735a8aa7055a6
dmchoi77 <dm.choi.dev@gmail.com>
2024-08-09 01:07:34 +0900

    add: storybook 컨트롤러 사용하기 위한 @storybook/addon-essentials 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,9 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
-import { join, dirname } from 'path';
-
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-essentials'],
+
   staticDirs: ['../public'],
   framework: '@storybook/nextjs',
 };
