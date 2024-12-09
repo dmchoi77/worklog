2307ccf6e7873c3dc49f51c73e5f622f93e98bd9
dmchoi77 <dm.choi.dev@gmail.com>
2024-12-10 00:14:25 +0900

    refactor: 404 페이지 tailwind로 변경

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,6 @@
 export default function Custom404() {
   return (
-    <div
-      css={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <div className='flex flex-col h-full justify-center items-center'>
       <span>잘못된 경로입니다.</span>
     </div>
   );
