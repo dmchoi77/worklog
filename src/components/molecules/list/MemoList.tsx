9209e99b986fa594f1afbccef04129667b70d515
dmchoi77 <dm.choi.dev@gmail.com>
2024-12-11 23:56:44 +0900

    fix: memeo 순서 바꾸는 로직 dnd -> motion으로 변경(react 19 업데이트 후 오동작 & beautiful dnd 종료)

--- src/components/molecules/list/MemoList.tsx
+++ src/components/molecules/list/MemoList.tsx
@@ -1,8 +1,7 @@
+import { useEffect, useState } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 
-import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
-
-import { List } from './CommonList';
+import { Reorder } from 'motion/react';
 import MemoCard from '../card/MemoCard';
 
 import { useSnackbarStore } from '~/stores/useSnackbarStore';
@@ -15,60 +14,30 @@ interface IProps {
 }
 
 export default function MemoList({ targetDate }: IProps) {
-  const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
 
-  const { data: memoList } = useFetchMemoList({ date: targetDate });
-  const { mutate: updateMemoOrder } = useUpdateMemoOrder();
+  const { data: memoList = [] } = useFetchMemoList({ date: targetDate });
+  const [item, setItem] = useState<IMemo[]>([]);
+
+  // const { mutate: updateMemoOrder } = useUpdateMemoOrder();
 
   const updateSnackbarState = useSnackbarStore((state) => state.updateSnackbarState);
 
-  const onDragEnd = async (result: DropResult) => {
-    const { destination, source, draggableId } = result;
-
-    if (!destination) return;
-    if (destination.droppableId === source.droppableId && destination.index === source.index) return;
-
-    const targetMemoId = Number(draggableId);
-    const desinationIndex = destination.index;
-    const orderedList = [...(memoList as IMemo[])];
-    const [reorderedItem] = orderedList.splice(source.index, 1);
-    orderedList.splice(desinationIndex, 0, reorderedItem);
-
-    queryClient.setQueryData(memoQueryKeys.fetchMemoList({ date: targetDate }).queryKey, orderedList);
-
-    updateMemoOrder(
-      { id: targetMemoId, order: desinationIndex },
-      {
-        onError: () => {
-          updateSnackbarState({
-            open: true,
-            horizontal: 'center',
-            message: '에러가 발생했습니다.',
-            vertical: 'bottom',
-          });
-        },
-
-        onSettled: () => {
-          queryClient.invalidateQueries(memoQueryKeys.fetchMemoList({}));
-        },
-      },
-    );
+  const reorderCallback = (newOrder: IMemo[]) => {
+    setItem(newOrder);
   };
 
+  useEffect(() => {
+    setItem(memoList);
+  }, [memoList]);
+
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId={'memo-list'}>
-        {(provided, snapshot) => {
-          return (
-            <List {...provided.droppableProps} ref={provided.innerRef} isDraggingOver={snapshot.isDraggingOver}>
-              {memoList?.map((memo, index) => (
-                <MemoCard key={memo.id} content={memo.content} id={memo.id} index={index} />
-              ))}
-              {provided.placeholder}
-            </List>
-          );
-        }}
-      </Droppable>
-    </DragDropContext>
+    <Reorder.Group axis='y' values={item} onReorder={reorderCallback}>
+      {item.map((memo) => (
+        <Reorder.Item key={memo.id} value={memo}>
+          <MemoCard content={memo.content} id={memo.id} />
+        </Reorder.Item>
+      ))}
+    </Reorder.Group>
   );
 }
