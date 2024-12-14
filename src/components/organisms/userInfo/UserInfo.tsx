2e49337a4d9bba1c2cf85568e327759adb590133
dmchoi77 <dm.choi.dev@gmail.com>
2024-12-14 23:51:32 +0900

    fix(userinfo): 아바타 이름 첫글자만 노출되도록 수정

--- src/components/organisms/userInfo/UserInfo.tsx
+++ src/components/organisms/userInfo/UserInfo.tsx
@@ -1,11 +1,11 @@
 'use client';
-import { Avatar } from '@radix-ui/themes';
 import { useEffect } from 'react';
-import { AccessToken } from '~/constants';
+import { Avatar } from '@radix-ui/themes';
 import { useLogout } from '~/queries/user';
 import { useUserInfoState } from '~/stores/useUserInfoStore';
 import { getCookie } from '~/utils/cookie';
 import { decodeJWT } from '~/utils/decodeJWT';
+import { AccessToken } from '~/constants';
 
 export const UserInfo = () => {
   const { username, updateUserInfoState } = useUserInfoState(({ updateUserInfoState, username }) => ({
@@ -27,7 +27,7 @@ export const UserInfo = () => {
 
   return (
     <div className='flex items-center gap-x-[8px]'>
-      <Avatar size='1' variant='solid' color='blue' radius='full' fallback={username} />
+      <Avatar size='1' variant='solid' color='blue' radius='full' fallback={username[0]} />
       <button onClick={() => handleLogout()}>
         <span className='text-[14px] font-[500]'>로그아웃</span>
       </button>
