f74703a332c6d964971ab351dc859e82b26de068
dmchoi77 <dm.choi.dev@gmail.com>
2024-12-26 23:55:55 +0900

    add: logo

--- src/components/templates/dashboard/DashboardLeft.tsx
+++ src/components/templates/dashboard/DashboardLeft.tsx
@@ -1,13 +1,16 @@
+import Image from 'next/image';
 import { Divider } from '@mui/material';
 import { DashboardLeftList } from './DashboardLeftList';
+import logo from '../../../../public/assets/images/logo_white.png';
 import SearchInput from '../../molecules/input/SearchInput';
 
 const DashboardLeft = () => (
   <div className='bg-[#303030] border-r-[1px] border-[#d5d5d552] min-w-[200px] max-w-[200px] overflow-y-auto'>
     <div className='flex h-[50px] items-center justify-center'>
-      <span className='text-[#F5F5F5] text-[16px] font-semibold'>오늘의 워크로그</span>
+      <Image src={logo.src} alt='logo' width={130} height={logo.height} />
     </div>
-    <SearchInput />
+    <hr className='bg-gray-400'/>
+    {/* <SearchInput /> */}
     <Divider />
     <DashboardLeftList />
   </div>
--- src/components/templates/login/LoginTitle.tsx
+++ src/components/templates/login/LoginTitle.tsx
@@ -1,7 +1,9 @@
+import Image from 'next/image';
+import Logo from '../../../../public/assets/images/logo.png';
+
 const LoginTitle = () => (
-  <div className='flex flex-col gap-[8px] pb-[30px]'>
-    <p className='text-[14px]'>오늘은 회사에서 어떤 일들이 펼쳐질까</p>
-    <span className='text-[34px] font-[700]'>오늘의 워크로그</span>
+  <div className='py-[20px]'>
+    <Image src={Logo.src} alt='logo' width={Logo.width} height={Logo.height} />
   </div>
 );
 
