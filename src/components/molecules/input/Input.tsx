1509a9d4a8e2da736c82230dabf333a2c44f7961
dmchoi77 <dm.choi.dev@gmail.com>
2025-02-01 00:32:01 +0900

    fix: Input 컴포넌트 스타일 수정

--- src/components/molecules/input/Input.tsx
+++ src/components/molecules/input/Input.tsx
@@ -6,17 +6,15 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(({ name, LeftIcon, RightIcon, ...props }, ref) => (
-  <div
-    className={[
-      'flex w-full h-[50px] bg-white rounded-[8px] border border-gray-300 align-middle items-center',
-      props.disabled && 'bg-gray-200',
-    ].join(' ')}
-  >
+  <div className={['flex w-full h-[50px] align-middle items-center', props.disabled && 'bg-gray-200'].join(' ')}>
     {LeftIcon && LeftIcon}
     <input
       name={name}
       ref={ref}
-      className={['w-full px-[12px] focus:outline-none', props.disabled && 'bg-gray-200'].join(' ')}
+      className={[
+        'w-full h-full px-[12px] rounded-[10px] border border-gray-300',
+        props.disabled && 'bg-gray-200',
+      ].join(' ')}
       {...props}
     />
     {RightIcon && RightIcon}
