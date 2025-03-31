4bfc6e61c00ce1f745de3e4009689224b08a84db
dmchoi77 <dm.choi.dev@gmail.com>
2025-04-01 00:08:06 +0900

    fix: 불필요 파일 제거

--- src/shared/utils/token.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-import { cookies } from 'next/headers';
-import { AccessToken } from '~/shared/constants';
-
-export const getAccessToken = () => {
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get(AccessToken)?.value;
-  return accessToken;
-};
