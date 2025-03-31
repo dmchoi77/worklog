24bd02148e30f01403bb8363805f7baafda27e80
dmchoi77 <dm.choi.dev@gmail.com>
2025-03-31 23:58:54 +0900

    refactor: 미들웨어 개선

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,34 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { AccessToken, RefreshToken } from '~/shared/constants';
+import { AccessToken } from '~/shared/constants';
 import { getTodayDate } from '~/shared/utils/date';
 
 export async function middleware(request: NextRequest) {
   const todayDate = getTodayDate();
+  const { nextUrl, url, cookies } = request;
 
-  const { nextUrl: url } = request;
+  const accessToken = cookies.get(AccessToken)?.value;
+  const hasToken = !!accessToken;
 
-  const accessToken = request.cookies.get(AccessToken)?.value;
-  const refreshToken = request.cookies.get(RefreshToken)?.value;
-  const isAuth = !!accessToken && !!refreshToken;
+  const isLoginPage = nextUrl.pathname === '/login';
+  const isMainPage = nextUrl.pathname === '/';
 
-  if (request.nextUrl.pathname === '/login' && isAuth) {
-    return NextResponse.redirect(new URL(`/content/${todayDate}`, request.url));
+  if (isLoginPage && hasToken) {
+    return NextResponse.redirect(new URL(`/content/${todayDate}`, url));
   }
 
-  if (request.nextUrl.pathname === '/' && !isAuth) {
-    return NextResponse.redirect(new URL('/login', request.url));
+  if (isMainPage && !hasToken) {
+    return NextResponse.redirect(new URL('/login', url));
   }
 
-  if (request.nextUrl.pathname !== '/login' && !isAuth) {
-    return NextResponse.redirect(new URL('/login', request.url));
+  if (!isLoginPage && !hasToken) {
+    return NextResponse.redirect(new URL('/login', url));
   }
 
-  if (request.nextUrl.pathname === '/' && isAuth) {
-    return NextResponse.redirect(new URL(`/content/${todayDate}`, request.url));
+  if (isMainPage && hasToken) {
+    return NextResponse.redirect(new URL(`/content/${todayDate}`, url));
   }
-  return NextResponse.rewrite(url);
+  return NextResponse.next();
 }
 
 export const config = {
