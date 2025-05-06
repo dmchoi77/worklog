bdc6d1f0cb563bc43f9fadff40b04cff5f3047b8
dmchoi77 <dm.choi.dev@gmail.com>
2025-05-06 23:45:39 +0900

    fix: WorkEdit 기능 분리

--- src/entities/work/ui/WorkCard.tsx
+++ src/entities/work/ui/WorkCard.tsx
@@ -1,26 +1,26 @@
 'use client';
 import { Checkbox } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { Work, WorkCategoryOptions } from '../api';
+import { Work, WorkCategory, WorkCategoryOptions } from '../api';
 import { WorkStatusButton } from './WorkStatusButton';
 import { Container } from '~/shared/ui/container/card.style';
 
 interface WorkCardProps {
   work: Work;
-  onCategoryChange?: (value: string) => void;
-  onStateToggle?: () => void;
+  onChangeCategory?: (value: WorkCategory) => void;
+  onChangeState?: (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => void;
   onDelete?: () => void;
   onDoubleClick?: () => void;
 }
 
-export const WorkCard = ({ work, onCategoryChange, onStateToggle, onDelete, onDoubleClick }: WorkCardProps) => (
+export const WorkCard = ({ work, onChangeCategory, onChangeState, onDelete, onDoubleClick }: WorkCardProps) => (
   <Container bgColor='lightblue' onDoubleClick={onDoubleClick}>
     <div className='flex items-center gap-x-[8px] text-ellipsis overflow-hidden'>
       <div className='m-[4px]'>
         <WorkStatusButton
           defaultOption={work.category}
           options={WorkCategoryOptions}
-          onSelectOption={onCategoryChange}
+          onSelectOption={onChangeCategory}
         />
       </div>
       <div className='overflow-auto'>
@@ -28,7 +28,7 @@ export const WorkCard = ({ work, onCategoryChange, onStateToggle, onDelete, onDo
       </div>
     </div>
     <div className='flex items-center'>
-      <Checkbox checked={work.state === 'COMPLETED'} onChange={onStateToggle} />
+      <Checkbox checked={work.state === 'COMPLETED'} onChange={onChangeState} />
       <DeleteIcon className='rounded-[6px] bg-[#ffffff]' onClick={onDelete} />
     </div>
   </Container>
--- src/features/work/ui/WorkCardEditable.tsx
+++ src/features/work/ui/WorkCardEditable.tsx
@@ -1,13 +1,11 @@
 'use client';
-import { useSearchParams } from 'next/navigation';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { useWorkMutation } from '../model';
 import { WorkDetail } from './WorkDetail';
-import { Work } from '~/entities/work/api';
+import { Work, WorkCategory } from '~/entities/work/api';
 import { workQueryKeys } from '~/entities/work/model/queries';
 import { WorkCard } from '~/entities/work/ui/WorkCard';
-import useWork from '~/shared/hooks/useWork';
 import { useSnackbarStore } from '~/shared/stores/useSnackbarStore';
 
 interface WorkCardEditableProps {
@@ -17,34 +15,12 @@ interface WorkCardEditableProps {
 export const WorkCardEditable = ({ work: initialWork }: WorkCardEditableProps) => {
   const [openWorkDetail, setOpenWorkDetail] = useState(false);
 
-  const { work, workSetter } = useWork(initialWork);
-
   const { updateWork, deleteWork } = useWorkMutation();
 
   const updateSnackbarState = useSnackbarStore((state) => state.updateSnackbarState);
   const queryClient = useQueryClient();
-  const selectedId = useSearchParams()?.get('selected');
 
-  useEffect(() => {
-    if (Number(selectedId) === initialWork.id) setOpenWorkDetail(true);
-  }, [initialWork.id]);
-
-  useEffect(() => {
-    if (Object.entries(initialWork).toString() === Object.entries(work).toString()) return;
-    updateWork(work, {
-      onSuccess: (data) => {
-        queryClient.invalidateQueries(workQueryKeys.fetchWorkList({}));
-        updateSnackbarState({
-          open: true,
-          message: data?.message,
-          horizontal: 'center',
-          vertical: 'bottom',
-        });
-      },
-    });
-  }, [work]);
-
-  const handleDelete = () =>
+  const onDelete = () =>
     deleteWork(
       { id: initialWork.id },
       {
@@ -56,13 +32,53 @@ export const WorkCardEditable = ({ work: initialWork }: WorkCardEditableProps) =
       },
     );
 
+  const onChangeCategory = (value: WorkCategory) => {
+    updateWork(
+      {
+        ...initialWork,
+        category: value,
+      },
+      {
+        onSuccess: (data) => {
+          queryClient.invalidateQueries(workQueryKeys.fetchWorkList({}));
+          updateSnackbarState({
+            open: true,
+            message: data?.message,
+            horizontal: 'center',
+            vertical: 'bottom',
+          });
+        },
+      },
+    );
+  };
+
+  const onChangeState = (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
+    const value = checked ? 'COMPLETED' : 'IN_PROGRESS';
+    updateWork(
+      {
+        ...initialWork,
+        state: value,
+      },
+      {
+        onSuccess: (data) => {
+          queryClient.invalidateQueries(workQueryKeys.fetchWorkList({}));
+          updateSnackbarState({
+            open: true,
+            message: data?.message,
+            horizontal: 'center',
+            vertical: 'bottom',
+          });
+        },
+      },
+    );
+  };
   return (
     <>
       <WorkCard
-        work={work}
-        onCategoryChange={workSetter('category')}
-        onStateToggle={() => workSetter('state')(work.state === 'COMPLETED' ? 'IN_PROGRESS' : 'COMPLETED')}
-        onDelete={handleDelete}
+        work={initialWork}
+        onChangeCategory={onChangeCategory}
+        onChangeState={onChangeState}
+        onDelete={onDelete}
         onDoubleClick={() => setOpenWorkDetail(true)}
       />
       {openWorkDetail && <WorkDetail {...initialWork} handleClose={() => setOpenWorkDetail(false)} />}
